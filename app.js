c677c3eddc0f2727cbb0474aca8e4a1301c194ec
Alexandria <alexandria.creek@gmail.com>
2020-08-19 22:09:30 -0700

    add

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var userName = prompt('Sign your soul away :D');
+
+alert('Do you like doggos?'+ username);
+
+if (userName === 'Alex'){
+    document.write('<h2>' + 'Welcome' + '</h2>')
+} else {
+    document.write('<h2>' + 'Welcome Friend :D' +</h2>)
+}
