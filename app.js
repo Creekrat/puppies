2f99a362aa99a6a1b9aa27c63df390fc150f5a34
Alexandria <alexandria.creek@gmail.com>
2020-08-20 12:12:04 -0700

    add

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 'use strict'
 
-var userName = prompt('Sign your soul away :D');
+function askUserForSoul(){
+    var userName = prompt('Sign your soul away :D');
+alert('Do you like doggos' + userName?);
+return userName;
+}
 
-alert('Do you like doggos?' + userName);
-
-if (userName === 'Alex'){
-    document.write('<h2>' + 'Welcome' + '</h2>')
-} else {
-    document.write('<h2>' + 'Welcome Friend :D' + '</h2>')
+function checkWelcome(){
+    
+    if (userName === 'Alex'){
+        document.write('<h2>' + 'Welcome' + '</h2>')
+    } else {
+        document.write('<h2>' + 'Welcome Friend :D' + '</h2>')
+    }
 }
