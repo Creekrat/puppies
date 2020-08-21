99f7f93e76f832b0eed7e4fea9f60a6a261c90a4
Alexandria <alexandria.creek@gmail.com>
2020-08-20 20:44:50 -0700

    game

--- app.js
+++ app.js
@@ -17,4 +17,27 @@ function checkWelcome(){
 
 var userName = askUserForSoul();
 
-checkWelcome();
+
+function numberGuessingGame() {
+    var correctAnswer = 25;
+    var playGame = prompt('Play a game with me?');
+    while(playGame === 'yes'){
+        for (var i = 0; i < 5; i = i + 1){
+            var userGuess = parseInt(prompt("Enter a number between 1-100"));
+            if(userGuess === correctAnswer){
+                alert('Yaaaaasss!')
+                break;
+            } else if (userGuess < correctAnswer){
+                alert('Not high enough..')
+            } else if (userGuess > correctAnswer){
+                alert('Whoa there! Not that high!')
+            }
+            if(i === 4){
+                alert('Fine here is the answer' + correctAnswer);
+            }
+        }
+        playGame = prompt('Wanna try again?');
+    }
+}
+
+numberGuessingGame();
